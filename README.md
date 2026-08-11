3fc39bbf7754794bcdc331674649a704f85580b8
dr.dimitru <ceo@veliov.com>
2026-08-11 12:44:01 +0300

    chore: scaffold ostrio-analytics-middleware package

---
+++ README.md
@@ -0,0 +1,3 @@
+# ostrio-analytics-middleware
+
+First-party Connect middleware proxy for [ostr.io](https://ostr.io/) web analytics beacons.
---
+++ src/types.ts
@@ -0,0 +1,36 @@
+import type { IncomingMessage } from 'node:http';
+import type { OstrioWebAnalyticsConfig } from 'ostrio-analytics';
+
+export interface OstrioAnalyticsMiddlewareConfig {
+  trackingId: string;
+  endpoint: string;
+  hostname: string;
+  serviceOrigin?: string;
+  defaultReferer?: string;
+  forwardedCookies?: string[];
+  responseHeaders?: string[];
+  upstreamTimeoutMs?: number;
+  wallTimeoutMs?: number;
+  maxSearchLen?: number;
+  resolveClientIp?: (req: IncomingMessage) => string | false;
+}
+
+export interface CreateTrackerOptions extends OstrioWebAnalyticsConfig {
+  trackingId: string;
+  endpoint: string;
+}
+
+export interface ResolvedMiddlewareConfig {
+  trackingId: string;
+  endpoint: string;
+  hostname: string;
+  beaconPath: string;
+  serviceOrigin: string;
+  defaultReferer: string;
+  forwardedCookies: Set<string>;
+  responseHeaders: Set<string>;
+  upstreamTimeoutMs: number;
+  wallTimeoutMs: number;
+  maxSearchLen: number;
+  resolveClientIp: (req: IncomingMessage) => string | false;
+}
